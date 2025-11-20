b7db1d1b0e77b4564bc8877081ed2a3871d12bf0
mariannesu-lakeforest <sum46@lakeforest.edu>
2025-11-19 18:41:50 -0600

    This os paper P2, it is about sorting algoirthms.

---
+++ README.java
@@ -0,0 +1,14 @@
+/** This is where the files for P2 will go to create the sorting algorithms.
+  * Heapsort
+  * Mergesort
+  * Quicksort
+  * Treesort
+  * Blocksort
+  *
+  * Updates will be made to GitHub on weekdays (REQUIRED), 
+  * failure to do so will result in dishonor and a lower grade probably.
+  *
+  * @param This is a readme file regarding sumary of instructions.
+  * @author Marianne Su
+**/
+
